8bda90022d487915cc92d2a28d618b326d77554e
Dominic Cerquetti <binary1230@gmail.com>
2015-08-17 20:25:01 -0400

    more fixes for barcode config + testing, not quite working yet.

--- barcode/site_sections/barcode.py
+++ barcode/site_sections/barcode.py
@@ -1,8 +1,22 @@
 from barcode import *
-
+from barcode_utils import generate_barcode_from_badge_num, get_badge_num_from_barcode
 
 # TODO: restrict to just admins for this stuff, probably.
 @all_renderable(c.PEOPLE)
 class Root:
     def allotments(self):
-        return "TEST"
+        badge_num = 3
+
+        encrypted = generate_barcode_from_badge_num(
+            badge_num=badge_num,
+            event_id=config['secret']['barcode_event_id'],
+            salt=config['secret']['barcode_salt'],
+            key=bytes(config['secret']['barcode_key'],'ascii'))
+
+        decrypted = get_badge_num_from_barcode(
+            barcode_num=encrypted,
+            event_id=config['secret']['barcode_event_id'],
+            salt=config['secret']['barcode_salt'],
+            key=bytes(config['secret']['barcode_key'],'ascii'))
+
+        return "badge_num = " + decrypted
