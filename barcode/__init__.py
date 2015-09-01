7db3f0356374ed5fb4f0530fd0017a4ac4167518
Dominic Cerquetti <binary1230@gmail.com>
2015-09-01 11:17:41 -0400

    barcode injection into badge exports

--- barcode/__init__.py
+++ barcode/__init__.py
@@ -1,7 +1,8 @@
 from uber.common import *
 from barcode._version import __version__
 from uber.reports import *
-from barcode_utils import generate_barcode_from_badge_num, get_badge_num_from_barcode
+from barcode import *
+from barcode.barcode_utils import generate_barcode_from_badge_num, get_badge_num_from_barcode
 
 config = parse_config(__file__)
 mount_site_sections(config['module_root'])
@@ -15,26 +16,29 @@ class BarcodeReportMixin:
     We will inject a barcode row into the exported data
     """
 
-    def write_personalized_badge_row(self, row, out):
+    def write_row(self, row, out):
         if self._include_badge_nums:
             # add in the barcodes here
             badge_num = row[0]
             barcode = generate_barcode_from_badge_num(badge_num)
             row.append(barcode)
 
-        return super.write_row(row, out)
+        # TODO: we should call the base class's writeRow(), but that's complex because of our multiple inheritance.
+        # this works fine for now.
+        out.writerow(row)
 
 
-class BarcodePrintedBadgeReport(uber.reports.PrintedBadgeReport, BarcodeReportMixin):
+class BarcodePrintedBadgeReport(BarcodeReportMixin, uber.reports.PrintedBadgeReport):
     pass
 
 
-class BarcodePersonalizedBadgeReport(uber.reports.PersonalizedBadgeReport, BarcodeReportMixin):
+class BarcodePersonalizedBadgeReport(BarcodeReportMixin, uber.reports.PersonalizedBadgeReport):
     pass
 
 
-class BarcodeSupporterBadgeReport(uber.reports.SupporterBadgeReport, BarcodeReportMixin):
+class BarcodeSupporterBadgeReport(BarcodeReportMixin, uber.reports.SupporterBadgeReport):
     pass
+
 
 uber.reports.printed_badge_report_type = BarcodePrintedBadgeReport
 uber.reports.personalized_badge_report_type = BarcodePersonalizedBadgeReport
