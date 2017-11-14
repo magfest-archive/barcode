1d1931b9d4ffb55f28feac8d687ab91ecac36d29
Rob Ruana <rob@robruana.com>
2017-11-14 03:47:23 -0500

    Updates barcode_api to use new token based authentication

--- barcode/barcode_api.py
+++ barcode/barcode_api.py
@@ -1,8 +1,10 @@
-from uber.api import fields as attendee_fields
+from uber.api import all_token_auth, AttendeeLookup
+from uber.server import register_jsonrpc
 from barcode import *
 from barcode.barcode_utils import get_badge_num_from_barcode
 
 
+@all_token_auth(c.API_READ)
 class BarcodeLookup:
     def lookup_attendee_from_barcode(self, barcode_value):
         with Session() as session:
@@ -16,7 +18,10 @@ class BarcodeLookup:
             # note: a descrypted barcode can yield to a valid badge#, but an attendee may not have that badge#
 
             attendee = session.query(Attendee).filter_by(badge_num=badge_num).first()
-            return attendee.to_dict(attendee_fields) if attendee else {'error': 'Valid barcode, but no attendee found with Badge #{}'.format(badge_num)}
+            if attendee:
+                return attendee.to_dict(AttendeeLookup.fields)
+            else:
+                return {'error': 'Valid barcode, but no attendee found with Badge #{}'.format(badge_num)}
 
     def lookup_badge_number_from_barcode(self, barcode_value):
         with Session() as session:
@@ -27,4 +32,4 @@ class BarcodeLookup:
                 return {'error': 'Couldn\'t look up barcode value: ' + str(e)}
 
 
-services.register(BarcodeLookup(), 'barcode')
+register_jsonrpc(BarcodeLookup(), 'barcode')
