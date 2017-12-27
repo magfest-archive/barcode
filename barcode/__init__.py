25ef06dba3ce1d2d41861b8c49b6916bcfab83e4
Victoria Earl <kitsuta@gmail.com>
2017-12-27 09:27:38 -0500

    Add badge num decryption decorator (#20)
    * Add a decorator to check for encrypted barcodes
    
    * Inject decorator over check_in and new_checkin
    
    * pep8 fix

--- barcode/__init__.py
+++ barcode/__init__.py
@@ -3,6 +3,7 @@ from barcode._version import __version__
 from barcode import *
 from barcode.barcode_utils import generate_barcode_from_badge_num, get_badge_num_from_barcode
 from barcode.barcode_api import *
+from uber.site_sections.registration import Root
 
 config = parse_config(__file__)
 
@@ -35,3 +36,27 @@ class BarcodePersonalizedBadgeReport(BarcodeReportMixin, uber.reports.Personaliz
 
 uber.reports.PrintedBadgeReport = BarcodePrintedBadgeReport
 uber.reports.PersonalizedBadgeReport = BarcodePersonalizedBadgeReport
+
+
+def check_for_encrypted_badge_num(func):
+    """
+    On some pages, we pass a 'badge_num' parameter that might EITHER be a literal
+    badge number or an encrypted value (i.e., from a barcode scanner). This
+    decorator searches for a 'badge_num' parameter and decrypts it if necessary.
+    """
+
+    @wraps(func)
+    def with_check(*args, **kwargs):
+        if kwargs.get('badge_num', None):
+            try:
+                int(kwargs['badge_num'])
+            except Exception:
+                kwargs['badge_num'] = get_badge_num_from_barcode(barcode_num=kwargs['badge_num'])['badge_num']
+        return func(*args, **kwargs)
+
+    return with_check
+
+check_in = check_for_encrypted_badge_num(Root.check_in)
+new_checkin = check_for_encrypted_badge_num(Root.new_checkin)
+Root.check_in = check_in
+Root.new_checkin = new_checkin
